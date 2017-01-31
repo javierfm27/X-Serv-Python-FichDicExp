d8cc2bfff7f634e0bafb91db45273106fc6bfb92
javierfm27 <javitofm27@github.com>
2017-01-31 18:39:40 +0100

    Esta es la version final del ejercicio del diccionario

--- diccionario.py
+++ diccionario.py
@@ -2,6 +2,12 @@
 fd=open("/etc/passwd")
 lineas=fd.readlines()
 fd.close()
+dicUser = {}
 for linea in lineas:
-    dic = {}
-    #print("login:" + (linea.split(":")[0]) + "  shell:" + linea.split(":")[-1][:-1])
+    dicUser[linea.split(":")[0]] = linea.split(":")[-1][:-1]
+#Now, we going to look the shell of user 'root' and 'imaginario'
+print("The shell of root is " + dicUser['root'])
+try:
+    print("The shell of imaginario is " + dicUser['imaginario'])
+except:
+    print("The user doesn't exist")
